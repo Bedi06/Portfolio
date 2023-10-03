9f2ed5b71469483c80baa0f2541cc9e0785ef131
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-10-03 14:42:58 +0100

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe";
 import DrawerAppBar from "./components/DrawerAppBar";
+import SideNav from ".//sidenav";
 
 function App() {
   return (
     <div className="App">
       <DrawerAppBar />
+      <AboutMe />
+      <SideNav />
     </div>
   );
 }
